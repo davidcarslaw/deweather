e467e8bbfa0f7be27b8e156bdcb09f9becd5ea93
David Carslaw <david.carslaw@york.ac.uk>
2017-12-17 09:00:30 +0000

    update trees number

--- R/metSim.R
+++ R/metSim.R
@@ -65,7 +65,7 @@ doPred <- function(mydata, mod, metVars) {
     ## new data with random samples
     mydata[metVars] <- lapply(mydata[metVars], function (x) x[id])
     
-    prediction <- predict.gbm(mod, mydata, 100)
+    prediction <- predict.gbm(mod, mydata, 250)
     prediction <- data.frame(date = mydata$date, pred = prediction)
     
     return(prediction)
