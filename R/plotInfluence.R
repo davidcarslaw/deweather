04443064a60c4f4c5febc42867fcb7cfaee41897
Jack Davison <davison.jack.jd@gmail.com>
2023-06-23 11:55:24 +0100

    feat: add extra functionality to `plotInfluence()`
    Adds the `col` argument to define bar colours, and the `sort` argument to sort the variables on the y axis

--- R/plotInfluence.R
+++ R/plotInfluence.R
@@ -1,20 +1,48 @@
 #' Plot a GBM influence plot
 #'
 #' @param dw_model Model object from running [buildMod()].
+#' @param sort Sort the variables by their relative variable influences?
+#'   Defaults to `TRUE`. `FALSE` displays them alphabetically.
+#' @param fill Colour to use to use to colour the bars. Alternatively, users can
+#'   provide "var" which will colour each bar differently, or "mean" which will
+#'   colour each bar by its mean relative variable influence.
 #' @export
 #' @return Plot
 #' @family deweather model plotting functions
 #' @author David Carslaw
-plotInfluence <- function(dw_model) {
+plotInfluence <- function(dw_model,
+                          col = "grey30",
+                          sort = TRUE) {
   if (!inherits(dw_model, "deweather")) {
     stop("Need to supply a deweather object from buildMod.")
   }
 
   ## extract influence data from object
   influ <- dw_model$influence
+  
+  # sort variables?
+  if (sort){
+    influ$var <- forcats::fct_reorder(influ$var, influ$mean)
+  }
+  
+  # deal with special colours
+  if (!col %in% c("var", "mean")) {
+    the_aes <- ggplot2::aes(y = .data$var, x = .data$mean)
+    bar <- ggplot2::geom_bar(stat = "identity", fill = col)
+    lab <- NULL
+  } else {
+    the_aes <- ggplot2::aes(y = .data$var, x = .data$mean, fill = .data[[col]])
+    bar <- ggplot2::geom_bar(stat = "identity")
+    lab <- switch (col,
+                   var = "variable",
+                   mean = "relative\ninfluence (%)"
+    )
+  }
 
-  ggplot2::ggplot(influ, ggplot2::aes(y = .data$var, x = .data$mean)) +
-    ggplot2::geom_bar(stat = "identity") +
-    ggplot2::xlab("relative variable influence (%)") +
-    ggplot2::ylab("variable")
+  ggplot2::ggplot(influ, mapping = the_aes) +
+    bar + 
+    ggplot2::labs(x = "relative variable influence (%)",
+                  y = "variable",
+                  fill = lab) +
+    ggplot2::scale_x_continuous(labels = scales::label_percent(scale = 1))
 }
